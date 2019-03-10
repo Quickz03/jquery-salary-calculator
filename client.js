3437471946f49a278e1b8fbbf7a020e6e36ab127
Brian Zilka <Bzilks@gmail.com>
2019-03-09 20:35:34 -0600

    made a constructor in addPerson function

--- client.js
+++ client.js
@@ -1,14 +1,22 @@
 $(document).ready(readyNow);
 
-function addItem(){
-    console.log('in addItem');
+function addPerson(){
+    console.log('in addPerson');
+    let newPerson= {
+        firstName: $( '#firstNameIn' ).val(),
+        lastName: $('#lastNameIn').val(),
+        idName: $('#idNameIn').val(),
+        jobTitle: $('#jobTitleIn').val(),
+        annualSalary: $('#annualSalaryIn').val()
+    }
+    console.log( 'in newPerson', newPerson );
+ 
     
 } // end addItem
 
 
 
 function readyNow(){
-    $( '#addInfo' ).on( 'click', addItem )
-
+    $( '#addInfo' ).on( 'click', addPerson)
 } // end readNow
 

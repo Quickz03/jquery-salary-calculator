1b507790d219ba11cff9c99cee89960e1657bece
Brian Zilka <Bzilks@gmail.com>
2019-03-09 18:57:52 -0600

    added some functions

--- client.js
+++ client.js
@@ -1,2 +1,14 @@
-$(document).ready(readyNow)
+$(document).ready(readyNow);
+
+function addItem(){
+    console.log('in addItem');
+    
+} // end addItem
+
+
+
+function readyNow(){
+    $( '#addInfo' ).on( 'click', addItem )
+
+} // end readNow
 
